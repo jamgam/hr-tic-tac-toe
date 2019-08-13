19fd1cd8eb1ed942844897f72cad3321a9eb6fab
Bryan Liang <bryanliang1995@gmail.com>
2019-08-13 08:43:25 -0700

    Have all features of a basic tic tac toe game

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 var boxes = document.getElementsByClassName('box');
 var whosTurn = 'x';
 
-var onClick = (e) => {
-  // console.log(e.target.children[0].innerHTML);
-  if (e.target.children[0] === undefined) {
-    console.log(e.target);
+var handleClicks = (e) => {
+  if (e.target.tagName === 'SPAN') {
+    onSpanClick(e, e.target);
+  } else if (e.target.className.includes('box')) {
+    onBoxClick(e);
+  } else if (e.target.className.includes('restart')) {
+    onRestartClick(e);
   }
+};
+
+var onRestartClick = (e) => {
+  whosTurn = 'x';
+  document.querySelectorAll('span').forEach((e) => {
+    e.innerHTML = '';
+  });
+  document.querySelectorAll('.box').forEach((e) => {
+    e.classList.remove('winningLine');
+  });
+  document.getElementById('winnertext').innerHTML = '';
+
+};
+var onSpanClick = (e) => {
+  whosTurn = e.target.innerHTML;
+  e.target.innerHTML = '';
+};
+
+var onBoxClick = (e) => {
   if (!e.target.children[0].innerHTML) {
     e.target.children[0].innerHTML = whosTurn;
     if (whosTurn === 'x') {
@@ -17,35 +39,54 @@ var onClick = (e) => {
     whosTurn = e.target.children[0].innerHTML;
     e.target.children[0].innerHTML = '';
   }
-  if (checkForWinner(getBoard())) {
-    let str = `${checkForWinner(getBoard()).toUpperCase()} HAS WON`;
-    document.getElementById('winnertext').innerHTML = str;
-  }
-  console.log(checkForWinner(getBoard()));
+  manageWinner();
 };
 
 var checkForWinner = (board) => {
   if (compareEquals(board[0], board[1], board[2])) {
-    return board[0];
+    return [board[0], 0, 1, 2];
   } else if (compareEquals(board[3], board[4], board[6])) {
-    return board[3];
+    return [board[3], 3, 4, 6];
   } else if (compareEquals(board[6], board[7], board[8])) {
-    return board[6];
+    return [board[6], 6, 7, 8];
   } else if (compareEquals(board[0], board[3], board[6])) {
-    return board[0];
+    return [board[0], 0, 3, 6];
   } else if (compareEquals(board[1], board[4], board[7])) {
-    return board[1];
+    return [board[1], 1, 4, 7];
   } else if (compareEquals(board[2], board[5], board[8])) {
-    return board[2];
+    return [board[2], 2, 5, 8];
   } else if (compareEquals(board[0], board[4], board[8])) {
-    return board[0];
+    return [board[0], 0, 4, 8];
   } else if (compareEquals(board[2], board[4], board[6])) {
-    return board[2];
+    return [board[2], 2, 4, 6];
   } else {
-    return '';
+    return [];
   }
 };
- 
+
+var manageWinner = () => {
+  var board = getBoard();
+  var tie = true;
+  for (let e of board) {
+    if (!e) {
+      tie = false;
+    }
+  }
+  if (tie) {
+    let str = 'TIE! NO ONE WON';
+    document.getElementById('winnertext').innerHTML = str;
+    return;
+  }
+  var winner = checkForWinner(board);
+  if (winner.length) {
+    let str = `${winner[0].toUpperCase()} HAS WON`;
+    for (let i = 1; i < winner.length; i++) {
+      document.getElementById(`${winner[i]}`).classList.add('winningLine');
+    }
+    document.getElementById('winnertext').innerHTML = str;
+  }
+};
+
 var compareEquals = (one, two, three) => {
   if (one === '' || two === '' || three === '') {
     return false;
@@ -57,8 +98,6 @@ var compareEquals = (one, two, three) => {
   }
 };
 
-
-
 var getBoard = () => {
   let board = [];
   for (let box of boxes) {
@@ -67,14 +106,5 @@ var getBoard = () => {
   return board;
 };
 
-
-for (let box of boxes) {
-  box.addEventListener('click', onClick);
-}
-
-// document.getElementsByClassName('box').addEventListener('click', onClick);
-
-// ele.appendChild(document.querySelector('#app'));
-// document.querySelector('#app').appendChild(ele);
-// document.getElementById('app').appendChild(ele);
+document.addEventListener('click', handleClicks);
 
